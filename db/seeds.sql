967cb4421399e147a0c5f410474ead5bba377726
afzama <141961264+afzama@users.noreply.github.com>
2023-11-16 17:03:48 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (department_name)
+VALUES  ("Sales"),
+        ("Legal"),
+        ("Finance"),
+        ("HR"),
+        ("Shipping"),
+        ("Engineering"),
+        ("Marketing"),
+        ("Operations");
+
+INSERT INTO role (title,salary,department_id)
+VALUES  ("Account Manager",60000, 1),
+        ("Contracts Counsel",75000, 2),
+        ("Financial Analyst",80000, 3),
+        ("HR Manager",90000, 4),
+        ("Logistics Coordinator",40000, 5),
+        ("DevOps Engineer",110000, 6),
+        ("Digital Marketing Analyst",85000, 7),
+        ("Corporate Analyst",70000, 8);
+
+INSERT INTO employee_names (first_name,last_name,role_id,manager_id)
+VALUES  ("John", "Huckle",1,NULL),
+        ("Matt", "Townes",2, 22),
+        ("Ava", "Grunge",3, 54),
+        ("Julie", "Smith",4, 72),
+        ("Ganesh", "Jayvareen",5, 17),
+        ("Xinwen", "Su",6, 7),
+        ("Susan", "Karp",7, 43),
+        ("Alex", "Sanchez",8, 60);
---
+++ server.js
@@ -0,0 +1,63 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'Chak443r!',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+);
+
+// Query database
+let deletedRow = 2;
+
+db.query(`DELETE FROM employee_department WHERE id = ?`, [2], (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+db.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+});
+
+const init = () => {
+    inquirer.prompt([{
+        type: "list",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a deparment", "Add a role", "Add an employee", "Update an employee role"],
+        Message: "What would you like to do?",
+        Name: "task"
+    }]).then(ans => {
+        if (ans.task === "View all departments") {
+            Connection.query('SELECT * FROM department', (err, results) => {
+                console.log(err);
+                console.table(results);
+                init()
+            })
+        }
+    });
+}
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
